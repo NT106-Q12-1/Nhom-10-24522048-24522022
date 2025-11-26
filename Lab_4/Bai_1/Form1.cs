1c2fc6a8b79bc0a805d105729556ff7065e80a67
Xuân Vương <doanlexuanvuong@gmail.com>
2025-11-26 13:34:52 +0700

    Hoàn thành bài 1

--- Lab_4/Bai_1/Form1.cs
+++ Lab_4/Bai_1/Form1.cs
@@ -1,3 +1,5 @@
+using System.Net;
+
 namespace Bai_1
 {
     public partial class Form1 : Form
@@ -5,6 +7,16 @@ namespace Bai_1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void bttn_get_Click(object sender, EventArgs e)
+        {
+            WebRequest req = WebRequest.Create(rtb_url.Text);
+            WebResponse res = req.GetResponse();
+            Stream data = res.GetResponseStream();
+            StreamReader sr = new StreamReader(data);
+            rtb_detail.Text = sr.ReadToEnd();
+            res.Close();
         }
     }
 }
